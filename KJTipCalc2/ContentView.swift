f4fb5335c63c5c1a5f8378ab8894850a18bb1761
Kristopher Johnson <kris@kristopherjohnson.net>
2019-06-08 05:27:35 -0400

    Initial Commit

---
+++ KJTipCalc2/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  KJTipCalc2
+//
+//  Created by Kristopher Johnson on 6/8/19.
+//  Copyright © 2019 Kristopher Johnson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
